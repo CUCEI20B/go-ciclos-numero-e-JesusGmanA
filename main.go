57524b2dd79e64a025088f3102127be7ad2e235b
JesusGmanA <jesus.guzman8982@alumnos.udg.mx>
2020-09-28 08:08:27 -0500

    Prueba

--- main.go
+++ main.go
@@ -2,6 +2,22 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+	var n int
+	fmt.Scan(&n)
+	var aux, factorial int
+	factorial = 1
+	aux = n
+	var sum float64
+	sum = 0
+	for i := 0; i <= n; i++ {
+		for j := aux; j > 0; j-- {
+			factorial *= j
+		}
+		sum += float64(1) / float64(factorial)
+		aux--
+		factorial = 1
+	}
+	fmt.Printf(" %f", sum)
+
 }
